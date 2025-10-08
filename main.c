536f3183dcc30a481ff27dc66b03a6567b3ca2e2
lacamillaa <12999356@itisrossi.vi.it>
2025-10-08 11:11:01 +0200

    created 'lettera' method to discern char types

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+int lettera(char c)
+{
+    if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') return 1;
+    return 0;
+}
+
 int main(void) {
     printf("Hello, World!\n");
     return 0;
