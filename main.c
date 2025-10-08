70251ed5d46bf89782995bb6f7af02500de2efb8
lacamillaa <12999356@itisrossi.vi.it>
2025-10-08 11:16:12 +0200

    'compare' function to ease string comparisons

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 
 int lettera(char c)
 {
-    if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') return 1;
+    if (c >= 'a' && c <= 'z') return -1;
+    if (c >= 'A' && c <= 'Z') return 1;
+    return 0;
+}
+
+int comp(char c1, char c2) {
+    if (lettera(c1) == lettera(c2) && c1 == c2) return 1;
+    if (lettera(c1) && lettera(c2) && (c1 + ' ' == c2 || c2 + ' ' == c1)) return 1;
     return 0;
 }
 
